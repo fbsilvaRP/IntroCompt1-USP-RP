69aadafcd4a370bcb248974090b75397f303b286
Felipe Braga <febragasilva@outlook.com>
2026-04-27 11:59:36 -0300

    mudanças

--- Laboratorio2.cpp
+++ Laboratorio2.cpp
@@ -3,10 +3,38 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-	//Definição de variáveis
-	float horaInicio, horaFinal; 
-	float duracaoJogo;
+	//Definição de variáveis:
+	float horaInicio = 0.0, horaFinal = 0.0; 
+	float duracaoJogo = 0.0;
+	
+	int funciona = 1;
+	
+	while(funciona)
+	{
+		//Obtendo as informações do usuário:
+		printf("Digite o valor numerico da hora de inicio do jogo: ");
+		scanf("%f%*c", &horaInicio);
+		
+		printf("Digite o valor numerico da hora de termino do jogo: "); 
+		scanf("%f%*c", &horaFinal);
+		
+		//Caso a duração do jogo se estenda para o dia seguinte:
+		if(horaFinal <= horaInicio)
+			duracaoJogo = (24 - horaInicio) + horaFinal;
+		else
+			duracaoJogo = horaFinal - horaInicio;
+			
+		//Saída do programa:
+		printf("O jogo durou %.2f horas", duracaoJogo);
+		
+		printf("\nDeseja calcular novamente? [0]NAO OU [1]SIM?: \nR: "); 
+		scanf("%d%*c", &funciona);
+		
+		//Apaga o console
+		system("cls");
+	}
 	
 }
