0e9aae77bfb28bdd920b445b4e1637356bdd43a7
Felipe Braga <febragasilva@outlook.com>
2026-04-27 11:24:12 -0300

    Create Laboratorio2.cpp

---
+++ Laboratorio2.cpp
@@ -0,0 +1,12 @@
+//Programa feito por Felipe Braga em 27 de abril de 2026
+//Laboratório 2 da disciplina de Introdução à Computação 1 
+
+
+#include <stdio.h>
+int main()
+{
+	//Definição de variáveis
+	float horaInicio, horaFinal; 
+	float duracaoJogo;
+	
+}
