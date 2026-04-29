9a96456a60ea02936d3d01a99cdf24302fcb713e
Felipe Braga <febragasilva@outlook.com>
2026-04-29 19:56:29 -0300

    Create Laboratorio3.cpp
    Começando o lab 3...

---
+++ Laboratorio3.cpp
@@ -0,0 +1,20 @@
+//Programa feito Por Felipe Braga em 29/04/2026.
+
+//Enunciado direto ao ponto:
+/*escrever um programa que, dados
+dois inteiros N e M, determine o número máximo de casas que
+podem receber números entre N e M, incluindo-os, sem dígitos
+repetidos.
+Entrada: Cada caso de teste é descrito em uma linha. A linha
+contém dois inteiros N e M, conforme descrito acima (100 ≤ N ≤ M
+≤ 999).
+Saída: Para cada caso de teste, imprima uma linha com um inteiro
+representando a quantidade de números de casas entre N e M,
+inclusive, sem dígitos repetidos.
+*/
+
+#include <stdio.h>
+int main()
+{
+	
+}
