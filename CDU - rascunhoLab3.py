d53d8967feced0eb6ef7f6a7cdca4761b0ce3df7
Felipe Braga <febragasilva@outlook.com>
2026-04-30 08:36:17 -0300

    Update CDU - rascunhoLab3.py
    ...

--- CDU - rascunhoLab3.py
+++ CDU - rascunhoLab3.py
@@ -1,6 +1,9 @@
+#feito por Felipe Braga em 30/04/2026
+
 print("Identificando unidade, dezena e centena de um valor")
 numero = int(input("Valor: "))
 
+'''
 if((numero > 0) and (numero < 10)):
     print(f"Algarismo da unidade: {numero}")
 
@@ -15,5 +18,19 @@ if((numero >= 100) and (numero < 1000)):
     print(f"Algarismo da dezena: {numero % 10}")
     numero = numero // 10
     print(f"Algarismo da centena: {numero % 10}")
+'''
 
 #Escrevendo de outra forma...
+unidade = numero % 10
+
+numero = numero // 10
+
+dezena = numero % 10
+
+numero = numero // 10
+
+centena = numero % 10
+
+print(f"Unidade: {unidade}")
+print(f"Dezena: {dezena}")
+print(f"Centena: {centena}")
